8bef4b29b5f3b32aa63f34cc4e9ce548aa84f852
Arin Gadre <aringadre@gmail.com>
2025-02-15 15:39:37 -0800

    fix: update Google OAuth callback URL and configuration

--- api/index.js
+++ api/index.js
@@ -61,7 +61,7 @@ let useridexport;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `${process.env.REACT_APP_BACKEND_URL}/auth/google/callback`
+    callbackURL: 'https://bingeflix-backend.onrender.com/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ googleId: profile.id });
