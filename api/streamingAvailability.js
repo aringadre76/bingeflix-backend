e6ce1670733a123cf007d2b63c5a63e7b8e71ce2
Arin Gadre <aringadre@gmail.com>
2025-02-15 22:56:06 -0800

    feat: add additional API key for streaming availability rotation

--- api/streamingAvailability.js
+++ api/streamingAvailability.js
@@ -33,6 +33,7 @@ async function fetchMovieData() {
 const API_KEYS = [
     'ecbd6ce98amsha24963bf3f9ee3bp19377ejsn9783a8efaa6b',  // regular key
     '151db67c49msh509c354b0dee5a3p19e99bjsnf78de8b1af13',  // other key
+    'cbc2c68d40msh729c93f9011f89dp17cb58jsn8bde115ac76b',  // new key
     // Add more API keys here if you have them
 ];
 
